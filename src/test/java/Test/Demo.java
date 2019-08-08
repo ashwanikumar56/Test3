371f2b59293ba7b6db8527fca1c2b899fc06dc00
ashwanikumar56 <ashwanikumar1qainfotwch.com>
2019-08-08 14:49:41 +0530

    Add remaning dependenciess

--- src/test/java/Test/Demo.java
+++ src/test/java/Test/Demo.java
@@ -2,6 +2,7 @@ package Test;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeTest;
 
 public class Demo 
@@ -15,7 +16,17 @@ public class Demo
 		   // declaration and instantiation of objects/variables  
 	    System.setProperty("webdriver.chrome.driver", "chrome/chromedriver"); 
 	    
-	    WebDriver driver=new ChromeDriver();  
+ 
+	    ChromeOptions options = new ChromeOptions();
+	    
+	    options.addArguments("start-maximized"); // open Browser in maximized mode
+	    options.addArguments("disable-infobars"); // disabling infobars
+	    options.addArguments("--disable-extensions"); // disabling extensions
+	    options.addArguments("--disable-gpu"); // applicable to windows os only
+	    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+	    options.addArguments("--no-sandbox"); // Bypass OS security model
+	    
+	    WebDriver driver=new ChromeDriver();
 	      
 	// Launch website  
 	    driver.navigate().to("http://www.google.com/");  
