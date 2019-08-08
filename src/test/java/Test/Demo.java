7151bf50e17197611618660f9788c402e0c2e668
ashwanikumar56 <ashwanikumar1qainfotwch.com>
2019-08-08 14:19:00 +0530

    Add remaning dependencies of pom

--- src/test/java/Test/Demo.java
+++ src/test/java/Test/Demo.java
@@ -3,7 +3,6 @@ package Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
 public class Demo 
 {
@@ -20,6 +19,7 @@ public class Demo
 	      
 	// Launch website  
 	    driver.navigate().to("http://www.google.com/");  
+	    System.out.println("hello");
 	}
 
 }
